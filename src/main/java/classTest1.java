9a0050274e479c8aa3ff4739be4fd4820ba0e2ea
maxpopovv <max.popovv@gmail.com>
2024-06-24 00:29:59 +0300

    связываем

---
+++ src/main/java/classTest1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: user
+ * Date: 24.06.24
+ * Time: 1:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class classTest1 {
+}
